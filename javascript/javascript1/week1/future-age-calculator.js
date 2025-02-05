1932e3a1e2d5b3358c2a4b4fab5333ff5a3460d4
Bayram <bayram9erdem@gmail.com>
2025-02-05 22:26:51 +0100

    future-age-calculator

---
+++ javascript/javascript1/week1/future-age-calculator.js
@@ -0,0 +1,11 @@
+console.log("==== Age-ify (A future age calculator) ====");
+
+// Shall we first not calculate the birth year based on the data we have :) ?
+
+let age = 40; // This is the future age of the person, so hard to say about his/her current age yet
+let yearFuture = 2027; //This is luckily what we know for sure. Phew!..
+
+let yearBirth = yearFuture - age; //so here i need to calculate the birth year
+console.log("You were born in " + yearBirth + ".");
+
+console.log("You will be " + age + " years old in " + yearFuture + ".");
