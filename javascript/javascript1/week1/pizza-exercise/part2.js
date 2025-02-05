50d1931b2de96cf2e8c139f6f4eace01e87937e2
Bayram <bayram9erdem@gmail.com>
2025-02-05 23:13:02 +0100

    pizza class exercises js

---
+++ javascript/javascript1/week1/pizza-exercise/part2.js
@@ -0,0 +1,53 @@
+console.log("==== PART 2 ====");
+const Owner = "Carlo";
+console.log("Owner:", Owner);
+const restaurant = "Bella Napoli";
+console.log("Restaurant:", restaurant);
+const specialOffer = "Pesto Pizza";
+console.log("Special Offer:", specialOffer);
+const description = "Experience the true Italian cuisine!";
+console.log("Description:", description);
+const address = "Nom Nom Street 3, 2300 Copenhagen";
+console.log("Address:", address);
+const openingTime = "10:00";
+console.log("Opening Time:", openingTime);
+const closingTime = "23:00";
+console.log("Closing Time:", closingTime);
+console.log("Welcome to", restaurant);
+console.log(description);
+const info =
+  "At Bella Napoli we have vegan options but unfortunately you're not allowed to smoke or bring dogs. Contact us on +45 12 34 56 78 or write us an email at bella-napoli@hyf.com";
+console.log(info);
+
+const pizza1 = "Margherita Pizza";
+const pizza1Price = 90;
+
+const pizza2 = "Pesto Pizza";
+const pizza2Price = 120;
+
+const pizza3 = "Pepperoni Pizza";
+const pizza3Price = 120;
+
+const pizza4 = "BBQ Chicken Pizza";
+const pizza4Price = 130;
+
+const pizza5 = "White Pizza";
+const pizza5Price = 110;
+
+console.log("Our Menu:");
+console.log("#1", pizza1, pizza1Price, "DKK");
+console.log("#2", pizza2, pizza2Price, "DKK");
+console.log("#3", pizza3, pizza3Price, "DKK");
+console.log("#4", pizza4, pizza4Price, "DKK");
+console.log("#5", pizza5, pizza5Price, "DKK");
+
+console.log("---> Updated Menu (the special offer temporarily removed) <---");
+
+console.log("#1", pizza1, pizza1Price, "DKK");
+const pizza2Updated = "Meat Lover's Pizza";
+const pizza2UpdatedPrice = 135;
+console.log("#2", pizza2Updated, pizza2UpdatedPrice, "DKK");
+console.log("#3", pizza3, pizza3Price, "DKK");
+console.log("#4", pizza4, pizza4Price, "DKK");
+console.log("#5", pizza5, pizza5Price, "DKK");
+console.warn("NEW HOURS! We are open between 12:00 - 21:00.");
