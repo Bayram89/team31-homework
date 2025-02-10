1c167a1d80ae107024a3c6b350fd18a6a61c397a
Bayram <bayram9erdem@gmail.com>
2025-02-10 14:02:45 +0100

    changes added

--- javascript/javascript1/week1/dog-age-calculator.js
+++ javascript/javascript1/week1/dog-age-calculator.js
@@ -2,16 +2,13 @@ console.log("==== Goodboy-Oldboy (A dog age calculator) ====");
 
 // First things first, I am expected to create the following variables. They are:
 
-let dogYearOfBirth; //When is the dog born? aka. Dog's birth year?
-dogYearOfBirth = 2021; //I put my dog's birth year here
+const dogYearOfBirth = 2021; //When is the dog born? aka. Dog's birth year? I put my dog's birth year here
 
-let dogYearFuture = 2045; //How old will the dog be in future year?
+const dogYearFuture = 2045; //How old will the dog be in future year?
 
-let dogYear; //After we have calculated dog's age AND one dog year is the equivalent of seven human years! Good to know!
-dogYear = dogYearFuture - dogYearOfBirth; //Here I calculate dog's age and multiply by 7 to find dog's age in human years
+const dogYear = dogYearFuture - dogYearOfBirth; //After we have calculated dog's age AND one dog year is the equivalent of seven human years! Good to know!
 
-let shouldShowResultInDogYears; //Here we use boolean variable due to T/F
-shouldShowResultInDogYears = true; //true = dog years, false = human years
+let shouldShowResultInDogYears = true; //Here we use boolean variable due to T/F
 
 if (shouldShowResultInDogYears) {
   console.log(
