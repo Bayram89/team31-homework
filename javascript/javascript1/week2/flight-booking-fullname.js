cb8fe781e97e21889a488b34ab505143d7da2597
Bayram <bayram9erdem@gmail.com>
2025-02-11 16:50:57 +0100

    changes on flight booking have been saved

---
+++ javascript/javascript1/week2/flight-booking-fullname.js
@@ -0,0 +1,11 @@
+// With writing the following function, I can combine firstname & surname of the passengers
+
+function getFullname(firstname, surname) {
+  return firstname + " " + surname;
+}
+
+let fullname1 = getFullname("Mustafa", "Adam");
+let fullname2 = getFullname("Johanna", "Eve");
+
+console.log(fullname1);
+console.log(fullname2);
