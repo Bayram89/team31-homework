79cd972ac1f0ce657198337c561b108373d5dbd8
Bayram <bayram9erdem@gmail.com>
2025-02-15 19:45:46 +0100

    changes added on flight booking

--- javascript/javascript1/week2/flight-booking-fullname.js
+++ javascript/javascript1/week2/flight-booking-fullname.js
@@ -4,8 +4,8 @@ function getFullname(firstname, surname) {
   return firstname + " " + surname;
 }
 
-let fullname1 = getFullname("Mustafa", "Adam");
-let fullname2 = getFullname("Johanna", "Eve");
+const fullname1 = getFullname("Mustafa", "Adam");
+const fullname2 = getFullname("Johanna", "Eve");
 
 console.log(fullname1);
 console.log(fullname2);
