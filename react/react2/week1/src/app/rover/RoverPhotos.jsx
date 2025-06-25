678b58a2cb23b8e1023a0971483d77613158c8f3
Bayram Erdem <bayram9erdem@gmail.com>
2025-06-25 19:09:57 +0200

    Added RoverPhotos component to display NASA Mars Rover photos

--- react/react2/week1/src/app/rover/RoverPhotos.jsx
+++ react/react2/week1/src/app/rover/RoverPhotos.jsx
@@ -1,4 +1,6 @@
-import React, { use, useState } from 'react'
+"use client";
+
+import React, { useEffect, useState } from 'react'
 
 export default function RoverPhotos() {
 
@@ -35,7 +37,23 @@ console.log('NASA Data:', nasaData);
 
   return (
     <div>
-      
+      <h1>NASA Rover Photos</h1>
+      <button onClick={fetchRoverPhotos}>Fetch Rover Photos</button>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {nasaData && (
+        <div>
+          <h2>Photos from Mars Rover Curiosity</h2>
+          <ul>
+            {nasaData.photos.map(photo => (
+              <li key={photo.id}>
+                <img src={photo.img_src} alt={`Mars Rover Photo ${photo.id}`} />
+                <p>Taken by: {photo.rover.name} on {photo.earth_date}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
